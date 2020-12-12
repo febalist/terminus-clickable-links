7a315b61720eca7de1420c8973ae1d011086ed6c
febalist <febalist@gmail.com>
2020-12-12 15:57:50 +0300

    phpstorm handler

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,76 +1,73 @@
-import * as fs from 'fs'
-const untildify = require('untildify')
-import { Injectable } from '@angular/core'
-import { ToastrService } from 'ngx-toastr'
-import { ElectronService } from 'terminus-core'
+import * as fs from 'fs';
+import {Injectable} from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {ElectronService} from 'terminus-core';
 
-import { LinkHandler } from './api'
+import {LinkHandler} from './api';
+
+const untildify = require('untildify');
 
 @Injectable()
 export class URLHandler extends LinkHandler {
-    // From https://daringfireball.net/2010/07/improved_regex_for_matching_urls
-    // See : https://stackoverflow.com/questions/6927719/url-regex-does-not-work-in-javascript
-    regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))/
+  // From https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+  // See : https://stackoverflow.com/questions/6927719/url-regex-does-not-work-in-javascript
+  regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))/;
 
-    priority = 5
+  priority = 5;
 
-    constructor (private electron: ElectronService) {
-        super()
-    }
+  constructor (private electron: ElectronService) {
+    super();
+  }
 
-    handle (uri: string) {
-        this.electron.shell.openExternal(uri)
-    }
+  handle (uri: string) {
+    this.electron.shell.openExternal(uri);
+  }
 }
 
 @Injectable()
 export class UnixFileHandler extends LinkHandler {
-    // Only absolute and home paths
-    regex = /(\/|~)+(\.|)[\w\/-]+(\.[\w]+|)/
+  // Only absolute and home paths
+  regex = /(\/|~)+(\.|)[\w\/-]+(\.[\w]+|)(:\d+)?/;
 
-    constructor (
-        private toastr: ToastrService,
-        private electron: ElectronService,
-    ) {
-        super()
-    }
+  constructor (
+      //private toastr: ToastrService,
+      private electron: ElectronService,
+  ) {
+    super();
+  }
 
-    convert (uri: string): string {
-        return untildify(uri)
-    }
+  convert (uri: string): string {
+    return untildify(uri);
+  }
 
-    handle (uri: string) {
-        if (!fs.existsSync(uri)) {
-            this.toastr.error('This path does not exist')
-            return
-        }
-        this.electron.shell.openExternal('file://' + uri)
-    }
+  handle (uri: string) {
+    const [file, line] = uri.split(':');
+    this.electron.shell.openExternal('phpstorm://open?file=' + file + '&line=' + line);
+  }
 }
-
 
 @Injectable()
 export class WindowsFileHandler extends LinkHandler {
-    // Only absolute and home paths
-    regex = /(([a-zA-Z]:|\\|~)\\[\w\-()\\\.]+|"([a-zA-Z]:|\\)\\[\w\s\-()\\\.]+")/
+  // Only absolute and home paths
+  regex = /(([a-zA-Z]:|\\|~)\\[\w\-()\\\.]+|"([a-zA-Z]:|\\)\\[\w\s\-()\\\.]+")/;
 
-    constructor (
-        private toastr: ToastrService,
-        private electron: ElectronService,
-    ) {
-        super()
-    }
+  constructor (
+      private toastr: ToastrService,
+      private electron: ElectronService,
+  ) {
+    super();
+  }
 
-    convert (uri: string): string {
-        const sanitizedUri = uri.replace(/"/g, '')
-        return untildify(sanitizedUri)
-    }
+  convert (uri: string): string {
+    const sanitizedUri = uri.replace(/"/g, '');
+    return untildify(sanitizedUri);
+  }
 
-    handle (uri: string) {
-        if (!fs.existsSync(uri)) {
-            this.toastr.error('This path does not exist')
-            return
-        }
-        this.electron.shell.openExternal('file://' + uri)
+  handle (uri: string) {
+    if (!fs.existsSync(uri)) {
+      this.toastr.error('This windows path does not exist');
+      return;
     }
+    this.electron.shell.openExternal('file://' + uri);
+  }
 }
